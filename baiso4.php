abbd79c38e1c7dd33b46762c0ad4d450b028d8e2
Dung Nguyen <nguyendung622@Dungs-MacBook-Air.local>
2019-10-01 08:50:07 +0700

    Bài học ngày 1/10

--- baiso4.php
+++ baiso4.php
@@ -4,8 +4,42 @@
 <?php
 #Code bài số 4
 include_once("model/book.php");
-$book = new Book(50, "OOP in PHP", "ndungithue", 2019);
-$book->display();
+$ls = Book::getList();
+$lsFromFile = Book::getListFromFile();
 ?>
+<div class="container pt-5">
+    <button class="btn btn-outline-info float-right"><i class="fas fa-plus-circle"></i> Thêm</button>
+    <table class="table mt-5">
+        <thead class="thead-dark">
+            <tr>
+                <th>STT</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Author</th>
+                <th>Year</th>
+                <th>Thao tác</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($lsFromFile as $key => $value) {
+                ?>
+                <tr>
+                    <td><?php echo $key ?></td>
+                    <td><?php echo $value->title ?></td>
+                    <td><?php echo $value->price ?></td>
+                    <td><?php echo $value->author ?></td>
+                    <td><?php echo $value->year ?></td>
+                    <td>
+                        <button class="btn btn-outline-warning"><i class="fas fa-pencil-alt"></i> Edit</button>
+                        <button class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i> Delete</button>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 
 <?php include_once("footer.php") ?>
--- model/book.php
+++ model/book.php
@@ -2,6 +2,7 @@
 class Book
 {
     #Begin properties
+    var $id;
     var $price;
     var $title;
     var $author;
@@ -9,8 +10,9 @@ class Book
     #end properties
 
     #Construct function
-    function __construct($price, $title, $author, $year)
+    function __construct($id, $title, $price, $author, $year)
     {
+        $this->id = $id;
         $this->price = $price;
         $this->title = $title;
         $this->author = $author;
@@ -24,5 +26,37 @@ class Book
         echo "Title: " . $this->title . "<br>";
         echo "Author: " . $this->author . "<br>";
         echo "Year: " . $this->year . "<br>";
+    }
+
+    #Mock data
+    /**
+     * Lấy toàn bộ các cuốn sách có trong CSDL
+     */
+    static function getList()
+    {
+        $listBook = array();
+        array_push($listBook, new Book(1, "OOP in PHP", 5, "ndung", 2015));
+        array_push($listBook, new Book(2, "OOP in C#", 8, "nha", 2017));
+        array_push($listBook, new Book(3, "OOP in Java", 10, "ntrung", 2018));
+        array_push($listBook, new Book(4, "OOP in Python", 20, "nlan", 2019));
+        array_push($listBook, new Book(5, "OOP in Ruby on Rails", 30, "thomas", 2019));
+
+        return $listBook;
+    }
+    /**
+     * Lấy dữ liệu từ file
+     */
+    static function getListFromFile()
+    {
+        $arrData = file("data/book.txt", FILE_SKIP_EMPTY_LINES);
+        $lsBook = array();
+        foreach ($arrData as $key => $value) {
+            $arrItem = explode("#", $value);
+            if (count($arrItem) == 5) {
+                $book = new Book($arrItem[0], $arrItem[1], $arrItem[2], $arrItem[3], $arrItem[4]);
+                array_push($lsBook, $book);
+            }
+        };
+        return $lsBook;
     }
 }
